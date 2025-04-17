fccff5cf5bae3f76c62afdc70707ba4abcdb2e3c
Rebeca Sindeaux <sindeauxrebeca@gmail.com>
2025-04-16 21:41:51 -0300

    Cadastro atualizado

--- ProjetoPOO/src/Cadastro/Usuarios.java
+++ ProjetoPOO/src/Cadastro/Usuarios.java
@@ -1,7 +1,39 @@
 package Cadastro;
 
+//eh recomendado que seja abstrata
 public class Usuarios {
-	//Teste para github
-	// se vc leu isso deu certo :D
+	private String nome;
+	private String email;
+	private String senha;
+	
+	public Usuarios(String nome, String email, String senha){
+		this.setNome(nome);
+		this.setEmail(email);
+		this.setSenha(senha);
+}
 
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
