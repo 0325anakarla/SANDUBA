ea72c46ed117b7d6235fe865e4d9aa59431c58bf
0325anakarla <anakarla2014.25@gmail.com>
2025-04-20 09:13:04 -0300

    Implementação de multiplas categorias

--- projetoSanduba/src/projetoSanduba/CategoriasJogos.java
+++ projetoSanduba/src/projetoSanduba/CategoriasJogos.java
@@ -1,7 +1,10 @@
 package projetoSanduba;
 
+import java.text.Normalizer;
+import java.util.Arrays;
+
 public enum CategoriasJogos { // Lista de categoria em Enum (enumeração),
-	
+
 	RPG("Role Playing Game"),
 	AVENTURA("Aventura"),
 	ACAO("Ação"),
@@ -9,7 +12,7 @@ public enum CategoriasJogos { // Lista de categoria em Enum (enumeração),
 	BEATEMUP("Beat Em' Up"),
 	SHOOTEMUP("Shoot Em' Up"),
 	METROIDVANIA("Metroidvania"),
-	CORRIDA("Corrida"), 
+	CORRIDA("Corrida"),
 	HORROR("Horror"),
 	FURTIVO("Furtivo"),
 	MUNDOABERTO("Mundo Aberto"),
@@ -40,15 +43,27 @@ public enum CategoriasJogos { // Lista de categoria em Enum (enumeração),
 	CARTAS("Cartas"),
 	RITMO("Ritmo"),
 	BATTLEROYALE("Battle Royale");
-	
+
 	private String categoria;
-	
+
 	CategoriasJogos(String categoria) {
 		this.categoria = categoria; // construtor padrao
 	}
-	
+
 	public String getCategoria() {
 		return this.categoria; // isso aqui é so pra imprimir a categoria bonitinho
 	}
-	
+
+	// criei um metodo no enum para converter as entradas dos usuarios
+	// aqui eu to usando a biblioteca java.util.stream no lugar de um loop normal com for
+	public static String[] converteString(String[] categoriasIvn){
+		return Arrays.stream(categoriasIvn)
+				.map(str -> Normalizer.normalize(str, Normalizer.Form.NFD)) // normalizer retira acentos
+				.map(str -> str.replaceAll("\\p{InCombiningDiacriticalMarks}+", "")) //limpa acentos
+				.map(str -> str.replaceAll("\\s+", "")) // retira espaço entre as palavras
+				.map(str -> str.trim().toUpperCase()) // retira espaço dos lados e deixa tudo maisculo
+				.toArray(String[]::new);
+	}
+	// no final as entradas vao ficar todas no padrao do Enum, por exemplo:
+	// entrada: "construção "; após a conversão: "CONSTRUCAO"
 }
--- projetoSanduba/src/projetoSanduba/Jogo.java
+++ projetoSanduba/src/projetoSanduba/Jogo.java
@@ -1,110 +1,128 @@
 package projetoSanduba;
 
-import java.text.Normalizer;
+import java.util.Arrays;
 
-public class Jogo {	
+public class Jogo {
 	private String titulo;
 	private double preco;
 	private String descricao;
-	private CategoriasJogos categoria;
+	private CategoriasJogos[] categoriasValidas;
 	private int classEtaria;
 	private String idiomas;
 	private String plataDisp;
 	private String modAtivacao;
-	
-	public Jogo(String titulo, double preco, String descricao, String categoria, int classEtaria, String idiomas,
-			String plataDisp, String modAtivacao) {
+
+	public Jogo(String titulo, double preco, String descricao, String[] categoriasInv, int classEtaria, String idiomas,
+				String plataDisp, String modAtivacao) {
 		super();
 		this.titulo = titulo;
 		this.preco = preco;
 		this.descricao = descricao;
-		categoria = Normalizer.normalize(categoria, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
-		// Pega o string escrito e retira acentuação
-		this.categoria = CategoriasJogos.valueOf(categoria.toUpperCase()); 
-		// vai converter o String categoria em valor Enum da list e depois colocar a palavra toda em letras maiusculas
-		// (no padrão da lista do Enum), assim as empresas vão poder escrever as categórias de várias formas e mesmo assim
-		// será cadastrado igual e de forma padronizada
+		String[] categoriasVal = CategoriasJogos.converteString(categoriasInv);
+		// pega o string escrito e chama o metodo do enum para converter
+		this.categoriasValidas = Arrays.stream(categoriasVal)
+				.map(CategoriasJogos::valueOf)
+				.toArray(CategoriasJogos[]::new);
+		// aqui ele esta armazenando cada String do categoriasVal dentro do categoriasValidas
 		this.classEtaria = classEtaria;
 		this.idiomas = idiomas;
 		this.plataDisp = plataDisp;
 		this.modAtivacao = modAtivacao;
 	}
-	
+
+	//criando um construtor apenas para teste
+	public Jogo(String titulo, String[] categoriasInv){
+		this.titulo = titulo;
+		String[] categoriasVal = CategoriasJogos.converteString(categoriasInv);
+		this.categoriasValidas = Arrays.stream(categoriasVal)
+				.map(CategoriasJogos::valueOf)
+				.toArray(CategoriasJogos[]::new);
+	}
+
 	public String getTitulo() {
 		return titulo;
 	}
-	
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-	
+
 	public double getPreco() {
 		return preco;
 	}
-	
+
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
-	
+
 	public String getDescricao() {
 		return descricao;
 	}
-	
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
-	/*public String getCategorias() { por causa do jeito que as categorias estão implementadas, a gente não vai usar get e set aqui
+
+	/*public String getCategorias() {
 		return categorias;
 	}
-	
+	a gente não vai usar esses get e set aqui
+	(lembrar de retirar isso depois)
 	public void setCategorias(String categorias) {
 		this.categorias = categorias;
 	}*/
-	
+
 	public int getClassEtaria() {
 		return classEtaria;
 	}
-	
+
 	public void setClassEtaria(int classEtaria) {
 		this.classEtaria = classEtaria;
 	}
-	
+
 	public String getIdiomas() {
 		return idiomas;
 	}
-	
+
 	public void setIdiomas(String idiomas) {
 		this.idiomas = idiomas;
 	}
-	
+
 	public String getPlataDisp() {
 		return plataDisp;
 	}
-	
+
 	public void setPlataDisp(String plataDisp) {
 		this.plataDisp = plataDisp;
 	}
-	
+
 	public String getModAtivacao() {
 		return modAtivacao;
 	}
-	
+
 	public void setModAtivacao(String modAtivacao) {
 		this.modAtivacao = modAtivacao;
 	}
-	
+
 	public void mostrarDados() {
 		System.out.println("Titulo: "+titulo);
 		System.out.println("Preço: "+preco);
 		System.out.println("Descrição: "+descricao);
-		System.out.println("Categorias: "+categoria.getCategoria());
+		System.out.println("Categorias: ");
+		System.out.println(Arrays.toString(categoriasValidas));
 		System.out.println("Classificação Etaria: "+classEtaria);
 		System.out.println("Idiomas Disponiveis: "+idiomas);
 		System.out.println("Plataformas: "+plataDisp);
 		System.out.println("Modo de ativação: "+modAtivacao);
 	}
-	
+
+	// criando outro mostrarDados so pra teste
+	public void mostrarDadosCat(){
+		System.out.println("Títutlo: "+titulo);
+		System.out.println("Categorias: ");
+		System.out.printf(Arrays.toString(categoriasValidas));
+	}
+
 	public boolean aplicaDescontoDe(double desconto) {
 		if (desconto > 0) {
 			preco = preco - preco * desconto;
